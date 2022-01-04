2d84afea12df1ac8dea40f92221db873f020e14f
rabelais88 <sungryeolp@gmail.com>
2022-01-04 17:29:17 +0900

    :alien: allowed find and findOne accessible for public role as default

--- src/api/contact/routes/contact.js
+++ src/api/contact/routes/contact.js
@@ -6,4 +6,6 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::contact.contact');
+module.exports = createCoreRouter('api::contact.contact', {
+  config: { find: { auth: false }, findOne: { auth: false } },
+});
--- src/api/work/routes/work.js
+++ src/api/work/routes/work.js
@@ -6,4 +6,6 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::work.work');
+module.exports = createCoreRouter('api::work.work', {
+  config: { find: { auth: false }, findOne: { auth: false } },
+});
