70b93f5c48b9722b9a4d4b7559201705789d4ce8
rabelais88 <sungryeolp@gmail.com>
2022-01-05 19:23:32 +0900

    :wrench: added reindexing options to environment values

--- README.md
+++ README.md
@@ -5,9 +5,20 @@
 $ DATABASE_URL=test yarn strapi $STRAPI_ARGS
 ```
 - 어드민 첫 접속시 반드시 public role에 대한 접근 권한을 허용해 줄 것
+- 다시 indexing이 필요하지 않을 경우 `.env`에서 `INDEXING_ON_BOOT=true`를 삭제할 것
 
 # endpoints
 ```
 $HOST_URL:1337/admin
 $HOST_URL:1337/graphql
+```
+
+# setting up & deploy
+```
+# https://devcenter.heroku.com/articles/git
+cp .env.example .env
+vi .env
+heroku git:remote -a sungryeol-portfolio
+# proceed next step after setting up heroku config vars
+git push heroku main
 ```
--- src/index.js
+++ src/index.js
@@ -18,20 +18,22 @@ module.exports = {
    * run jobs, or perform some special logic.
    */
   async bootstrap({ strapi }) {
-    algolia.init({
-      appId: process.env.ALGOLIA_APP_ID,
-      apiKey: process.env.ALGOLIA_API_KEY,
-    });
-    const posts = Array.from(
-      await strapi.db.query('api::post.post').findMany({
-        populate: {
-          tags: true,
-        },
-      })
-    ).map((p) => ({ ...p, objectID: p.id }));
-    console.log(`updating ${posts.length} post(s)...`);
-    // the Promise is left unwaited intentionally
-    await algolia.deleteObjects('posts');
-    algolia.saveObjects('posts', posts);
+    if (process.env.INDEXING_ON_BOOT === 'true') {
+      algolia.init({
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_API_KEY,
+      });
+      const posts = Array.from(
+        await strapi.db.query('api::post.post').findMany({
+          populate: {
+            tags: true,
+          },
+        })
+      ).map((p) => ({ ...p, objectID: p.id }));
+      console.log(`updating ${posts.length} post(s)...`);
+      // the Promise is left unwaited intentionally
+      await algolia.deleteObjects('posts');
+      algolia.saveObjects('posts', posts);
+    }
   },
 };
