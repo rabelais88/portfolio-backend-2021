84bcde8f510749ecf331a09e87e5153d9da01f82
rabelais88 <sungryeolp@gmail.com>
2022-01-08 01:42:59 +0900

    :bug: added missing requester support

--- src/updateHook.js
+++ src/updateHook.js
@@ -1,9 +1,11 @@
+const axios = require('axios');
+
 const updateHook = async () => {
   const hookUrl = process.env.VERCEL_CONTENT_CHANGE_HOOK_URL ?? '';
   console.log('post changed...');
   if (hookUrl !== '') {
     console.log('requesting FE redeploy via hook');
-    return await fetch(hookUrl, { method: 'POST' });
+    return await axios({ url: hookUrl, method: 'POST' });
   }
   console.log(
     'did not request for redeploy because VERCEL_CONTENT_CHANGE_HOOK_URL is missing'
