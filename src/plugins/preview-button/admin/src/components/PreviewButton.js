3c3cd5ba94c5761a94af73c1b078e5bb56f6ff1f
sungryeol park <sungryeolp@gmail.com>
2022-01-17 05:35:23 +0900

    preview mode added

--- src/plugins/preview-button/admin/src/components/PreviewButton.js
+++ src/plugins/preview-button/admin/src/components/PreviewButton.js
@@ -6,9 +6,12 @@ const PreviewButton = () => {
   if (slug !== 'api::post.post') return null;
   if (isCreatingEntry) return <div>cannot preview until saved</div>;
   const { uid } = modifiedData;
-  const url = `${PREVIEW_HOST_URL}/api/post-preview?preview=${PREVIEW_KEY}&uid=${uid}`;
+  // https://docs.strapi.io/developer-docs/latest/developer-resources/plugin-api-reference/admin-panel.html#available-actions
+  // The admin panel supports dotenv variables.
+  // All variables defined in a .env file and prefixed by STRAPI_ADMIN_ are available while customizing the admin panel through process.env.
+  const url = `${process.env.STRAPI_ADMIN_PREVIEW_HOST_URL}/api/post-preview?preview=${process.env.STRAPI_ADMIN_PREVIEW_KEY}&uid=${uid}`;
   return (
-    <a class="preview-button" href={url}>
+    <a className="preview-button" href={url}>
       preview this post
     </a>
   );
